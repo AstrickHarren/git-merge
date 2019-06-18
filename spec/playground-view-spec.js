51bedeb12a62ee2f4f05ec877f28fc52455cfde5
astrickharren <astrick_harren@hotmail.com>
2019-06-18 19:50:08 +0800

    init

---
+++ spec/playground-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PlaygroundView from '../lib/playground-view';
+
+describe('PlaygroundView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
