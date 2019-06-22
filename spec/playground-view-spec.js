73d644a2c2f2664ee8e077c27dbece1b695e12b8
astrickharren <astrick_harren@hotmail.com>
2019-06-22 15:21:35 +0800

    deleted unneccesaries

--- spec/playground-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import PlaygroundView from '../lib/playground-view';
-
-describe('PlaygroundView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
